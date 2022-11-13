7943a0f69a786671d3e869fff019a57218bf62bb
nicovio <nicolasviotti90@gmail.com>
2022-11-13 20:29:14 -0300

    Refactor

--- src/components/mundialAppBar.js
+++ src/components/mundialAppBar.js
@@ -1,10 +1,10 @@
 import EditIcon from '@mui/icons-material/Edit'
 import FlagIcon from '@mui/icons-material/Flag'
 import AppBar from '@mui/material/AppBar'
-import Button from '@mui/material/Button'
 import Toolbar from '@mui/material/Toolbar'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { IconButton } from './iconButton'
 
 export const MundialAppBar = () => {
     const navigate = useNavigate()
@@ -12,18 +12,8 @@ export const MundialAppBar = () => {
     return (
         <AppBar position="static" color='default'>
             <Toolbar>
-                <Button className="iconButton" onClick={() => navigate('/')}>
-                    <FlagIcon />
-                    <span>
-                        Buscar países
-                    </span>
-                </Button>
-                <Button className='iconButton' onClick={() => navigate('/fixture')}>
-                    <EditIcon />
-                    <span>
-                        ¡Cargá los resultados y mirá las posiciones!
-                    </span>
-                </Button>
+                <IconButton label="Buscar países" Icon={FlagIcon} onClick={() => navigate('/')} />
+                <IconButton label="¡Cargá los resultados y mirá las posiciones!" Icon={EditIcon} onClick={() => navigate('/fixture')} />
             </Toolbar>
         </AppBar>
     )
